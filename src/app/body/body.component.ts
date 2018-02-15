3dce0592b3e874125f95c5a00200bb64af4b1e36
trupti <hawaldartrupti@gmail.com>
2018-02-15 02:03:27 -0800

    Added dailymatters columns

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,11 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , trigger, state, style, transition, animate} from '@angular/core';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css']
+  styleUrls: ['./body.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(100%, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 export class BodyComponent implements OnInit {
+   menuState:string = 'out';
+toggleMenu() {
+    // 1-line if statement that toggles the value:
+    this.menuState = this.menuState === 'out' ? 'in' : 'out';
+  }
 
   constructor() { }
 
