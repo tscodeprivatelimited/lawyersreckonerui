a84472acc013a4069787b4e2e50345aed3180d86
trupti <hawaldartrupti@gmail.com>
2018-03-12 23:13:17 -0700

    added home menu buttons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,11 +8,25 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
+  // Doughnut
+  // public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  // public doughnutChartData:number[] = [350, 450, 100];
+  // public doughnutChartType:string = 'doughnut';
+
   constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
+  // events
+  // public chartClicked(e:any):void {
+  //   console.log(e);
+  // }
+ 
+  // public chartHovered(e:any):void {
+  //   console.log(e);
+  // }
+  
   toHome(){
     this.router.navigate(['/home']);    
   }
